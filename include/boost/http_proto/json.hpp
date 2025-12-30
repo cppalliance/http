668e6283905902444adab55417faa493b29993fd
Vinnie Falco <vinnie.falco@gmail.com>
2025-12-30 05:07:27 -0800

    Fix Boost.JSON reference for modular library

---
+++ include/boost/http_proto/json.hpp
@@ -0,0 +1,22 @@
+//
+// Copyright (c) 2025 Vinnie Falco (vinnie.falco@gmail.com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/cppalliance/http_proto
+//
+
+#ifndef BOOST_HTTP_PROTO_JSON_HPP
+#define BOOST_HTTP_PROTO_JSON_HPP
+
+#include <boost/http_proto/detail/config.hpp>
+#include <boost/json/value.hpp>
+
+namespace boost {
+namespace http_proto {
+
+} // http_proto
+} // boost
+
+#endif
